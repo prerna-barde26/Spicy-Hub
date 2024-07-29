dbae18238bec55495921b6164a9b9cfe50de2f86
K1shish09 <dadghayekashish@gmail.com>
2024-07-29 14:24:48 +0530

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+	ScrollReveal().reveal("header_content .header_btn",{
+        ...scrollRevealOption,
+		delay :1500;
+	});
+
+	ScrollReveal().reveal(".explore_image img,"{
+	...scrollRevealOption,
+	origin:"left",
+	});
+	
